fa5d79a57886fea151100c62e7c75c50ea612aef
kiku19 <kishorekumar1911999@gmail.com>
2024-07-29 16:33:04 +0530

    bug fixes

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,66 @@
-import path from 'path'
-import fs from 'fs'
+import path from "path";
+import fs from "fs";
 
-class Main{
+class Main {
+  private directoryPath: string = process.cwd();
 
-    private directoryPath:string = process.cwd() 
+  private serviceName: string = new Date().getTime().toString();
 
-    private serviceName:string = new Date().getTime().toString()
+  private processId =
+    process.env.PM2_ID ?? process.env.POD_NAME ?? this.serviceName;
 
-    private processId = process.env.PM2_ID??process.env.POD_NAME??this.serviceName
+  private rootPath: string = "";
 
-    private rootPath:string = ''
+  //SER DIRECTORY PATH AND SERVICE NAME
+  constructor(directoryPath?: string, serviceName?: string) {
+    directoryPath && (this.directoryPath = directoryPath);
+    serviceName && (this.serviceName = serviceName);
+    this.rootPath = path.join(this.directoryPath, "logker", this.serviceName);
+    this.initialize();
+  }
 
-    //SER DIRECTORY PATH AND SERVICE NAME
-    constructor(directoryPath?:string,serviceName?:string){
-        directoryPath && (this.directoryPath = directoryPath);
-        serviceName && (this.serviceName = serviceName);
-        this.rootPath = path.join(this.directoryPath,'logher',this.serviceName)
-        this.initialize()
+  private initialize() {
+    try {
+      if (!fs.existsSync(this.rootPath))
+        fs.mkdirSync(this.rootPath, { recursive: true });
+      this.overwriteConsoleFunctions();
+    } catch (error) {
+      throw error;
     }
+  }
 
-    private initialize(){
-        try{
-            if(!fs.existsSync(this.rootPath))
-                fs.mkdirSync(this.rootPath,{recursive:true})
-            this.overwriteConsoleFunctions()
-        }
-        catch(error){
-            throw error
-        }
-    }
+  private todaysDate() {
+    const day = new Date().getUTCDate().toString();
+    const month = (new Date().getUTCMonth() + 1).toString();
+    const year = new Date().getUTCFullYear().toString();
+    const date = `${month}-${day}-${year}`;
+    return date;
+  }
 
-    private todaysDate(){
-        const day = new Date().getUTCDate().toString()
-        const month = (new Date().getUTCMonth() + 1).toString()
-        const year = new Date().getUTCFullYear().toString()
-        const date = `${month}-${day}-${year}`
-        return date
-    }
+  private getCurrentDataPath() {
+    const currentDatePath = path.join(this.rootPath, this.todaysDate());
+    if (!fs.existsSync(currentDatePath)) fs.mkdirSync(currentDatePath);
+    return currentDatePath
+  }
 
-    private overwriteConsoleFunctions(){
-        const originalLog = console.log
-        console.log = (args)=>{
-            const currentDatePath = path.join(this.rootPath,this.todaysDate())
-            if(!fs.existsSync(currentDatePath))
-                fs.mkdirSync(currentDatePath)
-            fs.appendFileSync(path.join(currentDatePath,`${this.processId}.txt`),`${new Date()}: ${args}\n`)
-            originalLog(args)
-        }
-        const originalError = console.error
-        console.error = (args)=>{
-            const currentDatePath = path.join(this.rootPath,this.todaysDate())
-            if(!fs.existsSync(currentDatePath))
-                fs.mkdirSync(currentDatePath)
-            fs.appendFileSync(path.join(this.rootPath,`${this.processId}.txt`),`${new Date()}: ${args}\n`)
-            originalError(args)
-        }
-    }
+  private overwriteConsoleFunctions() {
+    const originalLog = console.log;
+    console.log = (args) => {
+      fs.appendFileSync(
+        path.join(this.getCurrentDataPath(), `${this.processId}.txt`),
+        `${new Date()}: ${args}\n`
+      );
+      originalLog(args);
+    };
+    const originalError = console.error;
+    console.error = (args) => {
+      fs.appendFileSync(
+        path.join(this.getCurrentDataPath(), `${this.processId}.txt`),
+        `${new Date()}: ${args}\n`
+      );
+      originalError(args);
+    };
+  }
 }
 
-export default Main
+export default Main;
